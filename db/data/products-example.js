00b2a4ee4a64912e50a9310d66e03e6813f3737d
Sevi <correosevi@gmail.com>
2022-03-21 20:20:39 +0000

    Create data folder & example of the data structure

---
+++ db/data/products-example.js
@@ -0,0 +1,10 @@
+//This is an example of the structure of the array of objects to populate the database.
+export const products = [
+  {
+    category: 'wine',
+    brand: 'wine-brand',
+    name: 'wine name',
+    status: 0,
+    favorite: false,
+  },
+];
