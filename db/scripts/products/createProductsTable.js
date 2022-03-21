d90f9b48b0d346934c04711f7e318eac8f232976
Sevi <correosevi@gmail.com>
2022-03-21 20:41:47 +0000

    Update create products & populate products scripts

--- db/scripts/products/createProductsTable.js
+++ db/scripts/products/createProductsTable.js
@@ -3,10 +3,11 @@ import query from '../../index.js';
 const userQuery = `CREATE TABLE IF NOT EXISTS products(
   id SERIAL PRIMARY KEY,
   name TEXT,
-  description TEXT,
+  brand TEXT,
   category TEXT,
-  price TEXT,
-  img TEXT,
+  size TEXT,
+  active BOOLEAN NOT NULL,
+  favorite BOOLEAN NOT NULL,
   timestamp TIMESTAMPTZ NOT NULL DEFAULT now()
   )`;
 
--- db/scripts/products/populateProductsTable.js
+++ db/scripts/products/populateProductsTable.js
@@ -1,21 +1,22 @@
 import query from '../../index.js';
-import { products } from '../../mockdata/mockdata-products.js';
+import { products } from '../../data/products.js';
 
 async function populateProductsTable() {
   for (let index = 0; index < products.length; index++) {
-    const { name, description, category, price, img } = products[index];
+    const { name, brand, category, size, active, favorite } = products[index];
 
     //Current time when data is added.
     const timestamp = 'now()';
 
     //SQL to populate the table.
-    const sqlString = `INSERT into products (name,description,category,price,img,timestamp) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *`;
+    const sqlString = `INSERT into products (name,brand,category,size,active,favorite,timestamp) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`;
     const response = await query(sqlString, [
       name,
-      description,
+      brand,
       category,
-      price,
-      img,
+      size,
+      active,
+      favorite,
       timestamp,
     ]);
     console.log(response);
